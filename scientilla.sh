880fde48a448e1318a4cd7164eeba8755a8ce48d
Dieter Casier <casier.dieter@gmail.com>
2023-10-18 16:33:30 +0200

    Add backend service to docker compose config

--- scientilla.sh
+++ scientilla.sh
@@ -2,7 +2,10 @@
 
 source .env
 
-DOCKER_COMPOSE=(docker-compose --project-name "${PROJECT_NAME}" -f docker-compose.yml)
+DOCKER_COMPOSE=(docker-compose --project-name "${PROJECT_NAME}" -f docker-compose.yml -f "docker-compose-${ENVIRONMENT}.yml")
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[0;37m'
 
 case $1 in
 start)
@@ -13,21 +16,49 @@ stop)
   "${DOCKER_COMPOSE[@]}" stop
   ;;
 
+down)
+  "${DOCKER_COMPOSE[@]}" down
+  ;;
+
 build)
   "${DOCKER_COMPOSE[@]}" down
   "${DOCKER_COMPOSE[@]}" build
-  ;;
-
-down)
-  "${DOCKER_COMPOSE[@]}" down
   ;;
 
 logs)
   "${DOCKER_COMPOSE[@]}" logs -f
   ;;
 
+restartb|restart-backend)
+  "${DOCKER_COMPOSE[@]}" restart backend
+  ;;
+
+logsb|logs-backend)
+  "${DOCKER_COMPOSE[@]}" logs -f backend
+  ;;
+
+bashb|bash-backend)
+  "${DOCKER_COMPOSE[@]}" exec backend /bin/sh
+  ;;
+
+npmb|npm-backend)
+  "${DOCKER_COMPOSE[@]}" run --rm backend npm "${@:2}"
+  ;;
+
 *)
-  echo "Command not found ..."
-  echo $"Usage: $1 {start|stop|build|down|logs}"
+  echo -e "${RED}Command not found ..."
+  echo -e ""
+  echo -e "${WHITE}List of commands:"
+  echo -e ""
+  echo -e "${GREEN}start                     ${WHITE}Create and start containers"
+  echo -e "${GREEN}stop                      ${WHITE}Stop running containers"
+  echo -e "${GREEN}down                      ${WHITE}Stop and remove containers & networks"
+  echo -e "${GREEN}build                     ${WHITE}Stop and remove containers & networks + build or rebuild services"
+  echo -e "${GREEN}logs                      ${WHITE}View output from containers"
+  echo -e "${GREEN}restartb, restart-backend ${WHITE}Restart backend service container"
+  echo -e "${GREEN}logsb, logs-backend       ${WHITE}View output from backend container"
+  echo -e "${GREEN}bashb, bash-backend       ${WHITE}Execute /bin/sh command in backend container"
+  echo -e "${GREEN}npmb, npm-backend         ${WHITE}Execute npm command in backend container"
+  echo -e ""
   ;;
 esac
